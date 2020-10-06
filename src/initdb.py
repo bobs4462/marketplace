b9065a50d269068bd98d1e131c13ccab3e65c4cf
Makhmudov Babur <bobs4462@gmail.com>
2020-10-06 22:44:14 +0300

    created and tested the database init script

--- src/initdb.py
+++ src/initdb.py
@@ -1,6 +1,60 @@
+#! /bin/python3
+import checks
+
+checks.version_check()
+
 from tables import BASE
+import json
 from db import DB
 
-db = DB('marketplace.db')
+db = DB(url='marketplace.db')
 
 BASE.metadata.create_all(db.get_engine())
+
+session = db.get_session()
+
+records = open('data/records.json') 
+
+# Loading data from file into a dictionary
+records = json.loads(records.read()) 
+
+# Adding User records
+from tables import User
+users = records['users']
+
+for u in users:
+    user = User(id=u['id'], name=u.get('name'), surname=u.get('surname'), patronymic=u.get('patronymic'), email=u['email'])
+    session.add(user) # adding record to table
+
+# Adding Book records
+from tables import Book
+books = records['books']
+for b in books:
+    book = Book(id=b['id'], name=b.get('name'), author=b.get('author'), isbn=b.get('isbn'))
+    session.add(book)
+
+# Adding Shop records
+from tables import Shop
+shops = records['shops']
+for s in shops:
+    shop = Shop(id=s['id'], name=s.get('name'), address=s.get('address'), zipcode=s.get('zipcode'))
+    session.add(shop)
+
+# Adding Order records
+from tables import Order
+from datetime import datetime as dt
+orders = records['orders']
+for o in orders:
+    order = Order(id=o['id'], reg_date=dt.strptime(o.get('reg_date'), '%Y-%m-%dT%H:%M'), user_id=o['user_id'])
+    session.add(order)
+
+# Adding OrderItem records
+from tables import OrderItem
+items = records['order_items']
+for i in items:
+    item = OrderItem(id=i['id'], order_id=i['order_id'], book_id=i['book_id'], shop_id=i['shop_id'], book_quantity=i['book_quantity'])
+    session.add(item)
+
+
+# Committing changes to database and closing connection
+session.commit()
--- src/tables.py
+++ src/tables.py
@@ -1,10 +1,11 @@
 from sqlalchemy.ext import declarative
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.types import DateTime
 
 # base class, sqlalchemy magic
 BASE = declarative.declarative_base()
 
-# All classed that are mapped to real database tables inherit from the base class,
+# All classes that are mapped to real database tables inherit from the base class,
 # sqlalchemy will then be able to describe database schema and map our classes accordingly.
 
 class User(BASE):
@@ -13,14 +14,14 @@ class User(BASE):
     name = Column(String)
     surname = Column(String)
     patronymic = Column(String)
-    email = Column(String)
+    email = Column(String, nullable=False, unique=True)
 
 class Book(BASE):
     __tablename__ = 'book'
     id = Column(Integer, primary_key=True)
     name = Column(String)
     author = Column(String)
-    isbn = Column(String)
+    isbn = Column(String, unique=True)
 
 class Shop(BASE):
     __tablename__ = 'shop'
@@ -33,12 +34,12 @@ class Order(BASE):
     __tablename__ = 'order'
     id = Column(Integer, primary_key=True)
     reg_date = Column(DateTime)
-    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 
 class OrderItem(BASE):
     __tablename__ = 'order_item'
     id = Column(Integer, primary_key=True)
-    order_id = Column(Integer, ForeignKey('order.id'))
-    book_id = Column(Integer, ForeignKey('book.id'))
-    book_quantity = Column(Integer)
-    shop_id = Column(Integer, ForeignKey('shop.id'))
+    order_id = Column(Integer, ForeignKey('order.id'), nullable=False)
+    book_id = Column(Integer, ForeignKey('book.id'), nullable=False)
+    book_quantity = Column(Integer, nullable=False)
+    shop_id = Column(Integer, ForeignKey('shop.id'), nullable=False)
