5cca2c2e80f49aaa3ef46e595aeed26562996c75
Bobur <bobs4462@gmail.com>
2020-10-07 10:59:50 +0300

    Added the function for order addition

--- src/helpers.py
+++ src/helpers.py
@@ -1,4 +1,5 @@
 import json
+from enum import Enum
 from copy import deepcopy
 
 # Below is a good example of how you should never write code
@@ -9,3 +10,19 @@ class Serializable:
         fields = deepcopy(self.__dict__)
         del fields['_sa_instance_state']
         return json.dumps(fields)
+
+class ValidationStatus(Enum):
+    OK = 'correct value'
+    IS_EMPTY = 'is not provided'
+    IS_NOT_INT = 'is not a valid integer'
+
+def validate_int(value) -> ValidationStatus:
+    # None check
+    if not value:
+        return ValidationStatus.IS_EMPTY
+    # let's check that requested value is a valid integer
+    if type(value) is not int:
+        return ValidationStatus.IS_NOT_INT
+    return ValidationStatus.OK
+
+
--- src/requests.py
+++ src/requests.py
@@ -1,27 +1,72 @@
 from aiohttp import web
+from helpers import validate_int, ValidationStatus
 from db import DB
 
 db = DB(url="marketplace.db")
 
 async def user(request: web.Request) -> web.Response:
-    user_id = request.query['id']
     response = None
-    # checking for absent user_id in request
-    if not user_id:
-        response = web.Response(status=400, reason='User\'s identificator is missing')
-    # let's check that requested id is a valid integer
-    try:
-        user_id = int(user_id)
-    except ValueError:
-        response = web.Response(status=400, reason='User\'s identificator is not a number')
-    session = db.get_session()
+    user_id = request.query['id']
+
+    # Perform some validation
+    status = validate_int(user_id)
+    if status != ValidationStatus.OK:
+        response = web.Response(status=400, reason=f'User\'s identificator {status}')
 
     if not response:
+        session = db.get_session()
         from tables import User
         user = session.query(User).filter_by(id=user_id).first()
         user = user.json_serialize()
         response = web.Response(body=user, content_type='application/json')
     return response
+
+async def order_add(request: web.Request) -> web.Response:
+    response = None
+
+    # Let's perform some validity checks first
+    book_id = request.query['book_id']
+    status = validate_int(book_id)
+    if status != ValidationStatus.OK:
+        response = web.Response(status=400, reason=f'Book\'s identificator {status}')
+    user_id = request.query['book_id']
+    status = validate_int(user_id)
+    if status != ValidationStatus.OK:
+        response = web.Response(status=400, reason=f'User\'s identificator {status}')
+    book_quantity = request.query['book_quantity']
+    status = validate_int(book_quantity)
+    if status != ValidationStatus.OK:
+        response = web.Response(status=400, reason=f'Book\'s quantity {status}')
+
+    shop_id = request.query['book_quantity']
+    status = validate_int(book_quantity)
+    if status != ValidationStatus.OK:
+        response = web.Response(status=400, reason=f'Book\'s quantity {status}')
+
+    # Creating order (from chaos :])
+    from tables import Order, OrderItem
+    from datetime import datetime
+    session = db.get_session()
+    order = Order(user_id=user_id, reg_date=datetime.now())
+    session.add(order)
+    session.flush()
+    order_item = OrderItem(order_id=order.id, book_id=book_id, book_quantity=book_quantity, shop_id=shop_id)
+    session.commit()
+
+    response = web.Response(status=200, reason=f'OK, order {order.id} added')
+
+
+async def user_orders_get(request: web.Request) -> web.Response:
+    response = None
+    user_id = request.query['id']
+    # Perform some validation
+    status = validate_int(user_id)
+    if status != ValidationStatus.OK:
+        response = web.Response(status=400, reason=f'User\'s identificator {status}')
+
+    if not response:
+        pass
+
 
 
 server = web.Application()
